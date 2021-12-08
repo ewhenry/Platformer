d8064653c38bff98cd65b698b367c86568d0524b
henry <77937076+ewhenry@users.noreply.github.com>
2021-12-08 02:55:58 -0500

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -8,67 +8,88 @@ var player, ground, platform;
 
 var playerImg, BGImg, platfromImg, groundImg;
 
+//IF TIME TRY THESE:
+//https://molleindustria.github.io/p5.play/examples/index.html?fileName=sprite9.js
+//
+
+class Player {
+  render() {
+    player = createSprite(400, 200);
+    player.addAnimation('normal', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
+    player.addAnimation('stretch', 'assets/asterisk_stretching0001.png', 'assets/asterisk_stretching0008.png');
+  }
+  movement() {
+    player.velocity.y += GRAVITY;
+
+    if (keyDown('UP') || keyWentDown('SPACE')) {
+      if (player.collide(platform) || player.position.y >= Level_H) {
+        player.changeAnimation('stretch');
+        player.animation.rewind();
+        player.velocity.y = -JUMP;
+      }
+    } else if (player.collide(platform) || player.position.y >= Level_H) {
+        player.velocity.y = 0;
+        player.changeAnimation('normal');
+        if (player.position.y >= Level_H) {
+          player.position.y = Level_H;
+        }
+        
+    }
+
+    if (keyDown('RIGHT')) {
+      player.velocity.x = 15;
+    } else if (keyDown('LEFT')) {
+      player.velocity.x = -15;
+    } else {
+      player.velocity.x = 0;
+    }
+
+    if (player.position.x < 0) {
+      player.position.x = 0;
+      //camera.position.x = player.position.x+200;
+    }
+    if (player.position.y < 0) {
+      player.position.y = 0;
+    }
+    if (player.position.x > Level_W) {
+      player.position.x = Level_W;
+    }
+  }
+}
+
+class Platform {
+  render(x, y) {
+    platform = createSprite(x, y);
+    platform.addAnimation('normal', 'assets/small_platform0001.png', 'assets/small_platform0003.png');
+  }
+
+
+}
+  
+let p = new Player();
+let plat = new Platform();
 function setup() {
   createCanvas(800,400);
-  camera.zoom = 0.5;
 
-  player = createSprite(400, 200);
-  player.addAnimation('normal', 'assets/asterisk_normal0001.png', 'assets/asterisk_normal0003.png');
-  player.addAnimation('stretch', 'assets/asterisk_stretching0001.png', 'assets/asterisk_stretching0008.png');
-  
-  platform = createSprite(400, 400);
-  platform.addAnimation('normal', 'assets/small_platform0001.png', 'assets/small_platform0003.png')
+  p.render();
+  plat.render(400,400);
+  plat.render(0,400);
+
+  camera.zoom = 0.5;
 }
 
 function draw() {
+
+  p.movement();
+  
   background(255,255,255);
+
   //console.log("PlayerXpos",player.position.x);
-  console.log("PlayerYpos",player.position.y);
+  //console.log("PlayerYpos",player.position.y);
 
-  if (player.collide(platform)) {
-    player.velocity.y = 0;
-    player.changeAnimation('normal');
-  }
-
-  if (keyWentDown('UP') || keyWentDown('SPACE') && player.collide(platform)) {
-    player.changeAnimation('stretch');
-    player.animation.rewind();
-    player.velocity.y = -JUMP;
-  }
-
-  if (keyWentDown('RIGHT')) {
-    player.changeAnimation('stretch');
-    player.animation.rewind();
-    player.position.x += JUMP*2;
-  }
-
-  if (keyWentDown('LEFT')) {
-    player.changeAnimation('stretch');
-    player.animation.rewind();
-    player.position.x -= JUMP*2;
-  } else {
-    player.velocity.y += GRAVITY;
-  }
-
-  //player.velocity.x = 1000;
 
   camera.position.x = player.position.x;
   camera.position.y = player.position.y;
-
-  if (player.position.x <= 0) {
-    player.position.x = 0;
-    //camera.position.x = player.position.x+200;
-  }
-  if (player.position.y <= 0) {
-    player.position.y = 0;
-  }
-  if (player.position.x > Level_W) {
-    player.position.x = Level_W;
-  }
-  if (player.position.y > Level_H) {
-    player.position.y = Level_H;
-    player.velocity.y = 0;
-  }
 
   drawSprites();
 }
